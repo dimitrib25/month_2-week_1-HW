420edc26ec06ed3d64fab7529efcd457864c06cf
dimitrib25 <154097411+dimitrib25@users.noreply.github.com>
2024-03-13 23:12:07 -0500

    iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -1,4 +1,11 @@
-#Create an IAM Role with S3 Read-Only Access:
+#Task 1
+resource "aws_iam_policy_attachment" "s3_read_only_access" {
+  name       = "S3ReadOnlyAccess"
+  roles      = [aws_iam_role.ReadOnlyS3AccessRole.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+#Task 2
 resource "aws_iam_role" "ReadOnlyS3AccessRole" {
   name = "ReadOnlyS3AccessRole"
 
@@ -17,8 +24,39 @@ resource "aws_iam_role" "ReadOnlyS3AccessRole" {
   })
 }
 
-resource "aws_iam_policy_attachment" "s3_read_only_access" {
-  name       = "S3ReadOnlyAccess"
+
+# Task 3
+resource "aws_iam_user" "s3Readeruser" {
+ name = "s3Readeruser"
+}
+
+resource "aws_iam_policy_attachment" "attachAssumeRole" {
+  name       = "attachAssumeRole"
+  users      = [aws_iam_user.s3Readeruser.name]
   roles      = [aws_iam_role.ReadOnlyS3AccessRole.name]
+  #groups     = [aws_iam_group.group.name]
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+output "Role_Arn" {
+  value = aws_iam_policy_attachment.attachAssumeRole.name
+ }
+
+ output "User_name" {
+  value = aws_iam_user.s3Readeruser.name
+ }
+
+ output "terraform_group" {
+  value = "Bushido Code"
+}
+output "Group_Members" {
+  value =<<-EOT
+   Lynn Parrish
+   Dimitri Burrell
+   Christian Akanni
+   Charlie Morse
+   Yahshua Emmanual Ball
+   Shawn Brown
+   Ahch Wordz
+EOT
 }
